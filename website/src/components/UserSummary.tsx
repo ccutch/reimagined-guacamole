59e9d4654a4aa576c5b1ed4fd5372fe11f54a1bb
Connor McCutcheon <connormccutcheon95@gmail.com>
2022-11-21 09:25:41 +0000

    Adding signout feature

--- website/src/components/UserSummary.tsx
+++ website/src/components/UserSummary.tsx
@@ -1,5 +1,5 @@
 import { format } from "date-fns"
-import { useSession } from "next-auth/react"
+import { signOut, useSession } from "next-auth/react"
 import { useEffect, useState } from "react"
 
 const DISCORD_DEFAULT_AVATAR = 'https://cdn.discordapp.com/embed/avatars/0.png'
@@ -31,8 +31,12 @@ const UserSummary: React.FC = () => {
             <div className='flex flex-col items-center w-80 h-min p-10 bg-gray-50 rounded-2xl shadow-md'>
                 <img src={data.user.image || DISCORD_DEFAULT_AVATAR} className="h-32 w-32 rounded-full" />
                 <h4 className='mt-5 font-bold text-2xl text-gray-800'>{data.user.name}</h4>
+                <button onClick={() => signOut()}
+                        className="my-3 px-6 py-2 font-semibold text-sm text-gray-500 bg-gray-100 hover:bg-gray-200 cursor-pointer rounded-full">
+                  Sign Out
+                </button>
 
-                <h2 className='mt-32 font-bold text-5xl text-gray-600'>{format(time, 'hh:mm aa')}</h2>
+                <h2 className='mt-24 font-bold text-5xl text-gray-600'>{format(time, 'hh:mm aa')}</h2>
                 <h5 className='mt-4 text-sm text-gray-700'>{format(time, 'MMMM dd, yyyy')}</h5>
             </div>
         </div>
